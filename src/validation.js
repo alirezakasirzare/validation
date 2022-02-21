ed97f6111af27a6bc282f24b6b666fdc3eac8fbc
Alireza Kasir <alirezakasir@yahoo.com>
2022-02-21 20:51:57 +0330

    create email method and get options

--- src/validation.js
+++ src/validation.js
@@ -5,13 +5,21 @@ class validation {
     lazy: true,
     required: true,
   };
-  in;
+  emailValidation = [];
   // constructor
   constructor(form) {
     this.formElement = this._$_(form);
   }
   // email validate
-  // email(inputId, option) {}
+  email(input, option = {}) {
+    const item = {
+      element: this._$_(input),
+      lazy: this._option_(option, "lazy"),
+      required: this._option_(option, "required"),
+    };
+
+    this.emailValidation.push(item);
+  }
   // select element method
   _$_(selector) {
     if (typeof selector === "string") {
@@ -19,5 +27,10 @@ class validation {
     } else {
       return selector;
     }
+  }
+  // get custom option
+  _option_(option, item) {
+    // console.log(item in option);
+    return item in option ? option[item] : this.defaultOptions[item];
   }
 }
